6b7ba267f4d93b837d3a247ad6faa5d0b31652c8
CoderX <73896715+AndreiAbd@users.noreply.github.com>
2022-02-18 08:23:14 +0200

    CoderX

---
+++ js/script.js
@@ -0,0 +1,36 @@
+/*
+Powered : CoderX | Andrei Abd
+█▀▀ █▀█ █▀▄ █▀▀ █▀█ ▀▄▀
+█▄▄ █▄█ █▄▀ ██▄ █▀▄ █░█
+CODER X |تم تصميم الموقع و برمجته من قبل اندريه عبد
+mail      :  usr.g0a1@gmial.com
+Whatsapp  :  +963934820511
+Pinterest :  https://www.pinterest.com/andreiabd93/
+Instagram :  https://www.instagram.com/andrei_abd/
+Linkeden  :  https://www.linkedin.com/in/andreiabd
+Facebook  :  https://www.facebook.com/andreiaabd/
+Github    :  https://github.com/AndreiAbd
+*/
+
+let
+    d = new Date();
+
+document.getElementById("addCurrentYear").innerHTML = d.getFullYear();
+
+//Loader Page
+window.addEventListener("load", function() {
+    const loader = document.querySelector(".loader");
+    loader.className += " hidden";
+});
+//Disable Right Mouse Click
+document.addEventListener('contextmenu', event => event.preventDefault());
+
+document.addEventListener("keydown", (e) => {
+    // USE THIS TO DISABLE CONTROL AND ALL FUNCTION KEYS
+    // if (e.ctrlKey || (e.keyCode>=112 && e.keyCode<=123)) {
+    // THIS WILL ONLY DISABLE CONTROL AND F12
+    if (e.ctrlKey || e.keyCode == 123) {
+        e.stopPropagation();
+        e.preventDefault();
+    }
+});
